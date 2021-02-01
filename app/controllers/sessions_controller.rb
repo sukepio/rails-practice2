ee3357ebfaa179995ce586e4d0de894670a588df
sukepio <naoto927sukena@gmail.com>
2021-02-01 13:50:29 +0000

    add validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required
+  
   def new
   end
   
   def create
-    user = User.find_by(emaill: session_params[:email])
+    user = User.find_by(email: session_params[:email])
     
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
@@ -21,6 +23,6 @@ class SessionsController < ApplicationController
   private
   
   def session_params
-    params.require(:user).permit(:name, :password)
+    params.require(:session).permit(:email, :password)
   end
 end
